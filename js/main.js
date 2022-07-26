72185658a2dc5a959d79648c4c1ebf587025d19f
FakhriddinovSh <shaxobiddinfaxriddinov@yandex.com>
2022-07-26 21:41:10 +0500

    ww

--- js/main.js
+++ js/main.js
@@ -1,2 +1,6 @@
-var elHamburger = document.querySelector(".site-header__menu-btn");
-var elSitenav = document.querySelector(".sitenav")
+var elHeader = document.querySelector(".site-header__inner")
+var elHamburger = document.querySelector(".site-header__menu-btn")
+
+elHamburger.addEventListener("click", function(){
+    elHeader.classList.toggle("site-header__inner--active")
+})
