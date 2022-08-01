62fb3933ac880857805776d3d8f333837b1d5c55
frontendTricks <fakhriddinovsh7@gmail.com>
2022-08-02 00:11:04 +0500

    Finish

--- js/main.js
+++ js/main.js
@@ -4,3 +4,26 @@ var elHamburger = document.querySelector(".site-header__menu-btn")
 elHamburger.addEventListener("click", function(){
     elHeader.classList.toggle("site-header__inner--active")
 })
+
+let elModal = document.querySelector(".modal")
+let elBtn = document.querySelector(".drink__btn")
+let elClose = document.querySelector(".modal__btn")
+let elCloseMobile = document.querySelector("#modal-close")
+let elHeroBtn = document.querySelector(".hero__btn")
+
+elBtn.addEventListener("click", function(){
+    elModal.classList.add("modal--active")
+})
+
+elClose.addEventListener("click", function(){
+    elModal.classList.remove("modal--active")
+})
+
+elCloseMobile.addEventListener("click", function(){
+    elModal.classList.remove("modal--active")
+})
+
+elHeroBtn.addEventListener("click", function(){
+    elModal.classList.add("modal--active")
+})
+
